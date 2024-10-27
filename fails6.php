3f2ac9ba4b9f339e56d1140e9b7c0436a96464e9
aVanadizns <arturs9398@gmail.com>
2024-10-27 14:21:58 +0200

    papildinām php failu

--- fails6.php
+++ fails6.php
@@ -1,3 +1,20 @@
 <?php
-echo "Sveika, pasaule!";
+// Funkcija sveicienam
+function sveiciens() {
+    echo "Sveika, pasaule!<br>";
+}
+
+// Funkcija skaitļu summas aprēķināšanai
+function summa($a, $b) {
+    return $a + $b;
+}
+
+// Izsauc sveiciena funkciju
+sveiciens();
+
+// Aprēķina un izvada summu
+$x = 5;
+$y = 10;
+echo "Skaitļu $x un $y summa ir: " . summa($x, $y);
 ?>
+
